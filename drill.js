fcbd6a01d507934c4d05d84b74497c580688dcbd
Park Byeong Jin <67573062+fatejin@users.noreply.github.com>
2021-02-16 13:48:27 +0900

    JavaScript event
    JavaScript event and function zero to one

---
+++ drill.js
@@ -0,0 +1,9 @@
+function showDetail() {
+    document.querySelector('#desc').style.display = "block"; // show detail
+    document.querySelector('#open').style.display = 'none'; // [show detail] button pause
+}
+
+function hideDetail() {
+    document.querySelector('#desc').style.display = "block"; // show detail
+    document.querySelector('#open').style.display = 'none'; // [show detail] button pause
+}
